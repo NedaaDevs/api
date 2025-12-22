8a9a3680f7627fd7ec3f239ed9ae85f70d965730
F2had <42780409+F2had@users.noreply.github.com>
2025-12-22 23:22:40 +0300

    feat: Favor env port value

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { app } from "@/app";
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 console.log(
 	`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
