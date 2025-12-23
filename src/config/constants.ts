b136e2539def97936ba257440a10f0353ca8d2a4
Fahad <42780409+F2had@users.noreply.github.com>
2025-12-23 11:31:43 +0300

    feat: Add constants for rate limiting and time live cache

---
+++ src/config/constants.ts
@@ -0,0 +1,15 @@
+export const RATE_LIMIT = {
+	GLOBAL: {
+		limit: 25,
+		duration: 60 * 1000, // 1 minute
+	},
+	REVERSE_GEOCODING: {
+		limit: 3,
+		duration: 60 * 1000 * 15, // 15 minutes
+	},
+} as const;
+
+export const CACHE_TTL = {
+	PRAYER_TIMES: 24 * 60 * 60 * 1000, // 24 hours,
+	LOCATION: 7 * 24 * 60 * 60 * 1000, // 7 days
+} as const;
