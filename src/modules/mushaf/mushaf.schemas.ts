4d3bd5010198e81294ee302254886a7e2c85abed
Fahad <42780409+F2had@users.noreply.github.com>
2026-03-11 10:34:50 +0300

    update mushaf config to font-based response

--- src/modules/mushaf/mushaf.schemas.ts
+++ src/modules/mushaf/mushaf.schemas.ts
@@ -2,13 +2,9 @@ import { type Static, t } from "elysia";
 
 export const MushafConfigResponse = t.Object({
 	version: t.Number(),
-	baseUrl: t.String(),
-	manifestUrl: t.String(),
-	pagesBaseUrl: t.String(),
-	imageWidth: t.Number(),
-	imageHeight: t.Number(),
-	totalPages: t.Number(),
+	fontsBaseUrl: t.String(),
 	totalSizeMB: t.Number(),
+	totalFonts: t.Number(),
 });
 
 export type MushafConfig = Static<typeof MushafConfigResponse>;
